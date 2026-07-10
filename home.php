974f5438a4aaa319f096ae990f7de14736187f31
itzflorence <angeloalmeda06@gmail.com>
2026-07-10 20:19:53 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="logo">LOGO</div>
+        
+        <input type="search" placeholder="Search what you need...">
+
+        <button type="icon-button">
+            <i class="fa-solid fa-bell"></i>
+        </button>
+
+        <button type="icon-button">
+            <i class="fa-solid fa-user"></i>
+        </button>
+    </nav>
+
+    <aside class="sidebar">
+        <div class="create-report">
+            <button>CREATE REPORT</button>
+        </div>
+
+        <div class="sidebar-options-wrapper">
+            <h4>THREADS</h4>
+            <div class="sidebar-options">
+                <a href="#">Active</a>
+                <a href="#">Resolved</a>
+                <a href="#">Archived</a>
+            </div>
+        </div>
+
+        <div class="sidebar-options-wrapper">
+            <h4>CATEGORIES</h4>
+            <div class="sidebar-options">
+                <a href="#">Flooding</a>
+                <a href="#">Car Crash</a>
+                <a href="#">Road Blockage</a>
+                <a href="#">Construction</a>
+                <a href="#">Other</a>
+            </div>
+        </div>
+
+        <div class="sidebar-options-wrapper">
+            <h4>MY ACTIVITY</h4>
+            <div class="sidebar-options">
+                <a href="#">My Reports</a>
+                <a href="#">Saved Reports</a>
+            </div>
+        </div>
+    </aside>
+    
+    <aside class="threads-wrapper">
+        
+    </aside>
+
+    <div class="main-wrapper">
+        <main>
+            post goes here
+        </main>
+    </div>
+</body>
+
+</html>
